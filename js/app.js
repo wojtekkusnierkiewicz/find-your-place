65d7c446117a99caa19c7a75d99e97014c7ff14c
Wojtek Kusnierkiewicz <wojtek.kusnierkiewicz@gmail.com>
2018-01-18 20:46:11 +0100

    adding second ajax and improving script

--- js/app.js
+++ js/app.js
@@ -11,18 +11,61 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $('.land').click(function(e) {
     e.preventDefault();
     var country = $(this).attr('title');
+    console.log(country);
+    if (country == 'United States') {
+      country = 'USA';
+    }
+    console.log(country);
     var countryUrl = "https://restcountries.eu/rest/v2/name/" + country;
-    $(this).css({fill:'red'})
+    var countryId  = $(this).attr('id');
     $('.land').not($(this)).css({fill:"#CCCCCC"});
     $('.popup').fadeIn(350);
 
     function insertCountry(country) {
       $('.one-country').remove();
-      var paragraph = $('<p>', {class: 'one-country'}).text(country.name);
-      div.prepend(paragraph);
+      var oneCountry = $('<div>', {class: 'one-country'});
+      var name = $('<h1>').text(country.name);
+      var capital = $('<h2>Capital city: <span>'+ country.capital + '</span></h2>')
+      var flag = $('<img>', {src:country.flag});
+      name.prepend(flag);
+      var region = $('<p>Region: <span>'+country.subregion+'</span></p>')
+      var area = $('<p>Area: <span>'+country.area +' km2</span></p>')
+      var nativName = $('<p>Native country name: <span>'+country.nativeName+'</span></p>')
+      var population = $('<p>Population: <span>'+(country.population.toLocaleString())+'</span></p>')
+      div.append(oneCountry);
+      oneCountry.append(name,capital,region,nativName,population,area);
+
+      function insertWeather(city) {
+        $('.one-weather').remove();
+        var oneWeather = $('<div>', {class: 'one-weather'});
+        var capital = $('<h2>Actual conditions in <span>'+city.name+'</span></h2>');
+        var icon = $('<img>', {src: 'http://openweathermap.org/img/w/'+city.weather[0].icon+'.png'});
+        var description = $('<span>'+city.weather[0].description+'</span>')
+        var iconP = $('<p>',{class: 'iconP'}).append(icon,description);
+        var claudiness = $('<p>Claudiness: <span>'+city.clouds.all+'%</span></p>')
+        var temp = $('<p>Temperature: <span>'+city.main.temp+' &deg; C</span></p>');
+        var pressure = $('<p>Pressure: <span>'+city.main.pressure+' hPa</span></p>');
+        var humidity = $('<p>Humidity: <span>'+city.main.humidity+'%</span></p>');
+        var DirTable = ["N","NNE","NE","ENE","E","ESE", "SE","SSE","S","SSW","SW","WSW", "W","WNW","NW","NNW","N"];
+        windDirection= DirTable[Math.floor((Number(city.wind.deg)+11.25)/22.5)];
+        var wind = $('<p>Wind: <span>speed: </span>'+city.wind.speed+' m/s <span>direction: </span>'+ windDirection+'</p>');
+        div.append(oneWeather);
+        oneWeather.append(capital,iconP,claudiness,temp,pressure, humidity,wind);
+      }
+
+      var capitalUrl = "http://api.openweathermap.org/data/2.5/weather?q="+country.capital+","+country.aplha2Code+"&units=metric"+"&APPID=bd5e378503939ddaee76f12ad7a97608";
+      function loadWeather() {
+        $.ajax({url: capitalUrl}).done(function(response){
+          console.log(response);
+          insertWeather(response)
+        }).fail(function(error){
+          console.log(error);
+        })
+      }
+      loadWeather();
     }
 
-    function loadDays() {
+    function loadCountry() {
       $.ajax({url: countryUrl}).done(function(response) {
         console.log(response[0]);
         insertCountry(response[0]);
@@ -30,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         console.log(error);
       })
     }
-    loadDays();
+    loadCountry();
   });
 
   $('form').submit(function(event) {
@@ -44,12 +87,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $('.popup').fadeIn(350);
 
     function insertCountry(country) {
+
       $('.one-country').remove();
-      var paragraph = $('<p>', {class: 'one-country'}).text(country.name);
-      div.prepend(paragraph);
-      var country = $("#" + country.alpha2Code).selector;
-      $(country).css({fill:'red'})
-      $('.land').not($(country)).css({fill:"#CCCCCC"});
+      var oneCountry = $('<div>', {class: 'one-country'});
+      var name = $('<h1>').text(country.name);
+      var capital = $('<h2>Capital city: <span>'+ country.capital + '</span></h2>')
+      var flag = $('<img>', {src:country.flag});
+      name.prepend(flag);
+      var region = $('<p>Region: <span>'+country.subregion+'</span></p>')
+      var area = $('<p>Area: <span>'+country.area +' km2</span></p>')
+      var nativName = $('<p>Native country name: <span>'+country.nativeName+'</span></p>')
+      var population = $('<p>Population: <span>'+(country.population.toLocaleString())+'</span></p>')
+      div.append(oneCountry);
+      oneCountry.append(name,capital,region,nativName,population,area);
+
+      var selected = country.alpha2Code;
+      $('#'+selected).css({fill:'red'})
+      $('.land').not($('#'+selected)).css({fill:"#CCCCCC"});
+
+      function insertWeather(city) {
+        $('.one-weather').remove();
+        var oneWeather = $('<div>', {class: 'one-weather'});
+        var capital = $('<h2>Actual conditions in <span>'+city.name+'</span></h2>');
+        var icon = $('<img>', {src: 'http://openweathermap.org/img/w/'+city.weather[0].icon+'.png'});
+        var description = $('<span>'+city.weather[0].description+'</span>')
+        var iconP = $('<p>',{class: 'iconP'}).append(icon,description);
+        var claudiness = $('<p>Claudiness: <span>'+city.clouds.all+'%</span></p>')
+        var temp = $('<p>Temperature: <span>'+city.main.temp+' &deg; C</span></p>');
+        var pressure = $('<p>Pressure: <span>'+city.main.pressure+' hPa</span></p>');
+        var humidity = $('<p>Humidity: <span>'+city.main.humidity+'%</span></p>');
+        var DirTable = ["N","NNE","NE","ENE","E","ESE", "SE","SSE","S","SSW","SW","WSW", "W","WNW","NW","NNW","N"];
+        windDirection= DirTable[Math.floor((Number(city.wind.deg)+11.25)/22.5)];
+        var wind = $('<p>Wind: <span>speed: </span>'+city.wind.speed+' m/s <span>direction: </span>'+ windDirection+'</p>');
+        div.append(oneWeather);
+        oneWeather.append(capital,iconP,claudiness,temp,pressure, humidity,wind);
+      }
+
+      var capitalUrl = "http://api.openweathermap.org/data/2.5/weather?q="+country.capital+","+country.aplha2Code+"&units=metric"+"&APPID=bd5e378503939ddaee76f12ad7a97608";
+      function loadWeather() {
+        $.ajax({url: capitalUrl}).done(function(response){
+          console.log(response);
+          insertWeather(response)
+        }).fail(function(error){
+          console.log(error);
+        })
+      }
+      loadWeather();
     }
 
     function loadDays() {
@@ -62,5 +145,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     loadDays();
-  })
+  });
+  // zooming
+  var svgElement = document.querySelector('svg');
+  var panZoomMap = svgPanZoom(svgElement);
+  panZoomMap.fit();
 });
